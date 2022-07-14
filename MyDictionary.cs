db76340dbf93ed2e3816a9b1da9d53adba7931b8
Muhammet-08 <79183003+Muhammet-08@users.noreply.github.com>
2022-07-14 17:20:21 +0300

    Add project files.

---
+++ MyDictionary.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<T>
+    {
+        T[] names;
+        T[] tempNames;
+
+        public MyDictionary()
+        {
+            names = new T[0];
+        }
+
+        public void Add(T name)
+        {
+            tempNames = names;
+            names = new T[names.Length + 1];
+            for (int i = 0; i < tempNames.Length; i++)
+            {
+                names[i] = tempNames[i];
+            }
+
+            names[names.Length-1] = name;
+        }
+
+        
+
+        public int Count
+        {
+            get { return names.Length; }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string> names1 = new MyDictionary<string>();
+            names1.Add("Muhammet");
+            names1.Add("Recep");
+            names1.Add("Kerem");
+            names1.Add("Barış");
+            names1.Add("Ahmet");
+            names1.Add("Mehmet");
+
+            Console.WriteLine(names1.Count);
+        }
+    }
+}
